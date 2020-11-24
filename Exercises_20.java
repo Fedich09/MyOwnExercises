5b542b0e9d288dcf346d6a0260ff33058257437d
Fedenko Maksym <fedenko.maxim.a@gmail.com>
2020-11-25 00:39:47 +0200

    added soulution Exercises 20

---
+++ Exercises_20.java
@@ -0,0 +1,32 @@
+/* Your goal is to return multiplication table for number that is always an integer from 1 to 10.
+
+For example, a multiplication table (string) for number == 5 looks like below:
+
+1 * 5 = 5
+2 * 5 = 10
+3 * 5 = 15
+4 * 5 = 20
+5 * 5 = 25
+6 * 5 = 30
+7 * 5 = 35
+8 * 5 = 40
+9 * 5 = 45
+10 * 5 = 50 */
+
+public class Exercises_20 {
+    public static void main(String[] args) {
+        System.out.println(multiTable(5));
+    }
+    public static String multiTable(int num) {
+        String s = "";
+        for (int i = 1; i < 11; i++) {
+            if (i == 10){
+                s += i + " * " + num + " = " + i * num;
+            }else {
+                s += i + " * " + num + " = " + i * num + "\n";
+            }
+
+        }
+        return s;
+    }
+}
